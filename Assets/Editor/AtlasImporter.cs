006d8bf9d1d4d3ee9efbd5e272e9a7bb6106cdb7
Jesper Hansen <jesper@jesperhansen.net>
2019-06-11 16:26:15 +0200

    Initial commit

---
+++ Assets/Editor/AtlasImporter.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class TexturePackerImporter : AssetPostprocessor
+{
+    public void OnPostprocessTexture(Texture2D texture)
+    {
+        if(Path.GetFileName(assetPath).Equals("atlas.png"))
+        {
+            TextureImporter importer = this.assetImporter as TextureImporter;
+
+            SpriteMetaData[] metadata = new SpriteMetaData[20 * 20];
+            for(int y = 0; y < 20; y++)
+            {
+                for(int x = 0; x < 20; x++)
+                {
+                    int i = x + y * 20;
+                    metadata[i].name = $"{x},{y}";
+                    metadata[i].rect.xMin = (float)texture.width * x / 20;
+                    metadata[i].rect.yMin = (float)texture.height * y / 20;
+                    metadata[i].rect.xMax = (float)texture.width * (x + 1) / 20;
+                    metadata[i].rect.yMax = (float)texture.width * (y + 1) / 20;
+                }
+            }
+
+            importer.textureType = TextureImporterType.Sprite;
+            importer.spriteImportMode = SpriteImportMode.Multiple;
+            importer.spritesheet = metadata;
+
+            EditorUtility.SetDirty(importer);
+        }
+    }
+}
---
+++ Assets/Editor/LevelImporter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+[ScriptedImporter(1, "lvl", 100)]
+public class LvlImporter : ScriptedImporter
+{
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        GameObject root = new GameObject("root");
+
+        Material spriteMaterial = new Material(Shader.Find("Sprites/Default"));
+
+        var atlasSprites = new List<Sprite>();
+        foreach(var asset in AssetDatabase.LoadAllAssetsAtPath("Assets/atlas.png"))
+        {
+            if(asset is Sprite sprite)
+                atlasSprites.Add(sprite);
+        }
+
+        for(int i = 0; i < 1000; i++)
+        {
+            GameObject go = new GameObject($"sprite {i}");
+            go.transform.SetParent(root.transform, false);
+            go.transform.localPosition = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0.0f);
+            go.transform.localScale = new Vector3(2, 2, 2);
+
+            SpriteRenderer spriteRenderer = go.AddComponent<SpriteRenderer>();
+            spriteRenderer.material = spriteMaterial;
+            spriteRenderer.sprite = atlasSprites[Random.Range(0, atlasSprites.Count)];
+        }
+        ctx.AddObjectToAsset("level", root);
+        ctx.SetMainObject(root);
+        ctx.AddObjectToAsset("material", spriteMaterial);
+    }
+}
